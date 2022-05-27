9dbbd211b010750840a76d57210c71e3f047970a
Sadid369 <sadid.jones@gmail.com>
2022-05-27 14:23:13 +0600

    @27/05/2022 @  Creating Models

---
+++ playground/views.py
@@ -0,0 +1,11 @@
+from tkinter import Y
+from django.shortcuts import render
+from django.http import HttpResponse
+def calculate():
+    x =1
+    y=2
+    return x
+    
+def say_hello(request):
+    x=calculate()
+    return render(request, 'hello.html', {'name':'Sadid'})
